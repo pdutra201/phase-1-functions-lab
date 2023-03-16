573ef6f151f1671db2104d7de9a66528577060f5
Philip Dutra <monsterphilip10@gmail.com>
2023-03-16 11:32:01 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    let numBlocks = Math.abs(location - 42);
+    return numBlocks;
+}
+
+function distanceFromHqInFeet(location){
+    //distanceFromHqInBlocks(location);
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(block1, block2){
+    let numFeet = Math.abs(block1 - block2) * 264;
+    return numFeet;
+}
+function calculatesFarePrice(block1, block2){
+    let distance = distanceTravelledInFeet(block1, block2);
+    if(distance < 400) {
+        return 0;
+    }
+    else if(distance <= 2000){
+        return (distance - 400) * 0.02;
+    }
+    else if(distance <= 2500){
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+
+    
+}
